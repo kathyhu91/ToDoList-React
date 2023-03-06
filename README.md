ac08ad66e92f4dc43b675dcf2b0a37b938af900a
Kimi Hu <kimihu@Kimide-MacBook-Air.local>
2023-03-06 16:19:07 +0800

    project initialization, adding and editing functions of the list

---
+++ README.md
@@ -0,0 +1,20 @@
+# Todo List with React
+
+- 開發使用[Create React App] (https://github.com/facebook/create-react-app)
+
+## 使用本專案
+
+1. clone 本專案 repo
+   `git clone https://github.com/kathyhu91/react-ToDoList.git`
+
+2. 安裝 npm
+   ` npm init`
+
+3. 專案啟動
+   ` npm start`
+
+4. 瀏覽本專案網頁
+   `於瀏覽器打開路徑 http://localhost:3000 `
+
+5. 停止專案執行
+   `Ctrl+c`
---
+++ src/pages/TodoPage.jsx
@@ -0,0 +1,138 @@
+import { useState } from 'react';
+import { Footer, Header, TodoCollection, TodoInput } from 'components';
+
+const dummyUserName = 'Tina';
+const dummyTodos = [
+  {
+    title: 'Learn react-router',
+    isDone: true,
+    id: 1,
+  },
+  {
+    title: 'Learn to create custom hooks',
+    isDone: false,
+    id: 2,
+  },
+  {
+    title: 'Learn to use context',
+    isDone: true,
+    id: 3,
+  },
+  {
+    title: 'Learn to implement auth',
+    isDone: false,
+    id: 4,
+  },
+];
+
+const TodoPage = () => {
+  const [todos, setTodos] = useState(dummyTodos);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleChange = (value) => {
+    setInputValue(value);
+  };
+
+  const handleAddTodo = () => {
+    if (inputValue.length === 0) {
+      return;
+    }
+    setTodos((prevTodos) => {
+      return [
+        ...prevTodos,
+        {
+          id: Math.random() * 100,
+          title: inputValue,
+          isDone: false,
+        },
+      ];
+    });
+    setInputValue('');
+  };
+
+  const handleKeyDown = () => {
+    if (inputValue.length === 0) {
+      return;
+    }
+
+    setTodos((prevTodos) => {
+      return [
+        ...prevTodos,
+        {
+          id: Math.random() * 100,
+          title: inputValue,
+          isDone: false,
+        },
+      ];
+    });
+
+    setInputValue('');
+  };
+
+  const handleToggleDone = (id) => {
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            isDone: !todo.isDone,
+          };
+        }
+        return todo;
+      });
+    });
+  };
+  const handleChangeMode = ({ id, isEdit }) => {
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            isEdit,
+          };
+        }
+
+        return { ...todo, isEdit: false };
+      });
+    });
+  };
+
+  const handleSave = ({ id, title }) => {
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            id,
+            title,
+            isEdit: false,
+          };
+        }
+
+        return todo;
+      });
+    });
+  };
+
+  return (
+    <div>
+      TodoPage
+      <Header username={dummyUserName} />
+      <TodoInput
+        inputValue={inputValue}
+        onChange={handleChange}
+        onAddTodo={handleAddTodo}
+        onKeyDown={handleKeyDown}
+      />
+      <TodoCollection
+        todos={todos}
+        onSave={handleSave}
+        onToggleDone={handleToggleDone}
+        onChangeMode={handleChangeMode}
+      />
+      <Footer />
+    </div>
+  );
+};
+
+export default TodoPage;
